5136bcf39deec43b27b90b44964b2bcf3b1b754c
Olivier Martin <olimartin90@gmail.com>
2018-05-31 22:38:33 -0400

    Initial commit with all my work so far Javascripting Module 3

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+var fruitLength = fruit.length;
+
+if (fruitLength > 5) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has five characters or less.');
+}
